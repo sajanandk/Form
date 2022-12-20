4e23f34d020f7206c40475d81c43e4ba4165550c
sajanand <sajanandkandgule67@gmail.com>
2022-12-20 18:32:39 +0530

    Add project files.

---
+++ Models/Employee.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Form.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public city city { get; set; }
+        public skills skills { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+        public bool AgreeTerm { get; set; }
+    }
+}
+
+public enum city
+{
+    Delhi,
+    Mumbai,
+    Kolkata,
+    Chennai,
+    Bangalore
+}
+public enum skills
+{
+    HTML5,
+    CSS3,
+    Bootstrap,
+    JavaScript,
+    JQuery,
+    Angular,
+    MVC,
+    WebAPI
+}
